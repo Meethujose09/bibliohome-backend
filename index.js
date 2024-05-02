87a7f63a9fc22b0360a36051c4c8f4b3af0d95ef
Meethu Jose <meethujose09@gmail.com>
2024-05-02 22:45:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bookRoute = require("./routes/book.routes.js");
+const wishlistRoute = require("./routes/wishlist.routes.js");
+const app = express();
+const cors = require('cors');
+
+
+
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// Enable CORS for all routes
+app.use(cors());
+
+// routes
+app.use("/api/books", bookRoute);
+app.use("/api/wishlist", wishlistRoute);
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
+
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://meethuJose09:Nkm28lk7Xn8Ze8HV@bibliohome.omomurn.mongodb.net/?retryWrites=true&w=majority&appName=biblioHome"
+      
+    );
+    app.listen(8090, () => console.log("Server started on port 8090"));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
+module.exports = app;
+
+
+  
